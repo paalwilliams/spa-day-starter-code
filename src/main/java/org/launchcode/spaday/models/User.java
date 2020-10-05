8fb1116ddcb78c5dcc4a1e541b2ba7c3e4363513
paalwilliams <paal@pwmd.biz>
2020-10-05 18:49:35 -0500

    Finish Studio 13 w/ Bonus Mission

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,9 +1,28 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.*;
+
 public class User {
+
+    @NotBlank
+    @Size(min=5, max=15, message="Username must be between 5 and 15 characters.")
     private String username;
+
+    @Email(message="Email address invalid")
     private String email;
+
+    @NotBlank
+    @Size(min=6, message="Password must be at least 6 characters")
     private String password;
+
+    @NotNull(message = "Passwords do not match")
+    private String verifyPassword;
+
+    private void checkPassword() {
+        if(this.password != null && this.verifyPassword != null && !this.password.equals(verifyPassword)){
+            this.verifyPassword = null;
+        }
+    }
 
     public User() {
 
@@ -38,5 +57,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
